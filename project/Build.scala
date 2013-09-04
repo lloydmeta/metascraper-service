3ef97ed779adf2b8e8b3358aeb3ec7e9bb91a15f
Lloyd <lloydmeta@gmail.com>
2013-09-04 15:23:53 +0900

    Formatting

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-      scalaVersion := "2.10.2"
+    scalaVersion := "2.10.2"
   )
 
 }
