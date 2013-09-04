0308d1a63b433092cffd9b9a2034187f9cf8588c
Lloyd <lloydmeta@gmail.com>
2013-09-04 12:12:14 +0900

    Scala version

--- project/Build.scala
+++ project/Build.scala
@@ -10,13 +10,14 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "com.beachape.metascraper" %% "metascraper" % "0.0.2"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-      Keys.scalaVersion := "2.10.2"
+      scalaVersion := "2.10.2"
   )
 
 }
