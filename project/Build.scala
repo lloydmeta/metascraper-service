5a8458bb0cde901dc50910917d61c3cbf7cdc2d8
Lloyd Meta <lloydmeta@gmail.com>
2013-09-06 08:59:26 +0900

    Use snapshot for now

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "com.beachape.metascraper" %% "metascraper" % "0.0.3",
+    "com.beachape.metascraper" %% "metascraper" % "0.0.3-SNAPSHOT",
     "org.scalatest" %% "scalatest" % "1.9.1" % "test"
   )
 
