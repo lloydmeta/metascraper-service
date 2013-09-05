08935506e1461458bf8be07c68210490bfa76515
Lloyd Meta <lloydmeta@gmail.com>
2013-09-06 07:58:28 +0900

    Use newer version of metascraper

--- project/Build.scala
+++ project/Build.scala
@@ -5,13 +5,13 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "metascrape-service"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "1.1-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "com.beachape.metascraper" %% "metascraper" % "0.0.2",
+    "com.beachape.metascraper" %% "metascraper" % "0.0.3",
     "org.scalatest" %% "scalatest" % "1.9.1" % "test"
   )
 
