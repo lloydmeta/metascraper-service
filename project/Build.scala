b1c8adbb2ab585f5dede3a995a0dae2019fc2d2f
Lloyd Meta <lloydmeta@gmail.com>
2013-09-11 08:30:22 +0900

    Use 0.0.9

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "com.beachape.metascraper" %% "metascraper" % "0.0.6",
+    "com.beachape.metascraper" %% "metascraper" % "0.0.9",
     "org.scalatest" %% "scalatest" % "1.9.1" % "test"
   )
 
