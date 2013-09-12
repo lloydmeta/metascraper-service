b41ba92afa8c194a76801a260f4185fd952e07bf
Lloyd <lloydmeta@gmail.com>
2013-09-12 10:35:25 +0900

    Up metascraper dependency version

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     jdbc,
     anorm,
-    "com.beachape.metascraper" %% "metascraper" % "0.1.1",
+    "com.beachape.metascraper" %% "metascraper" % "0.2.2",
     "org.scalatest" %% "scalatest" % "1.9.1" % "test"
   )
 
